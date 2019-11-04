ce001014722faad86050d4ffc616f4c6a29300ee
Blijinhui <1455098147@qq.com>
2019-11-04 14:12:07 +0800

    ljh提交11.4

--- src/api/index.js
+++ src/api/index.js
@@ -8,10 +8,49 @@ export async function get_homedata(params, config = {}) {
     return data;
 }
 
+
 export async function get_detaildata(datas, config = {}) {
     let { data } = await bsk.get('/detail_inf/detail', {
         params: {
             Name: datas
+        }
+    });
+    return data;
+}
+
+export async function check_phone(datas, config = {}) {
+    let { data } = await bsk.get('/reg/check', {
+        params: {
+            phone: datas
+        }
+    });
+    return data;
+}
+
+export async function create_inf(datas, config = {}) {
+    let { data } = await bsk.get('/create_inf/create', {
+        params: {
+            phone: datas.phone,
+            password: datas.password
+        }
+    });
+    return data;
+}
+
+export async function check_login(datas, config = {}) {
+    let { data } = await bsk.get('/reg/check_login', {
+        params: {
+            logPhone: datas.logPhone,
+            logPass: datas.logPass
+        }
+    });
+    return data;
+}
+// 未登录前渲染本地的购物车
+export async function logout_cart(datas, config = {}) {
+    let { data } = await bsk.get('/logout_goods/cart', {
+        params: {
+            til_list: datas,
         }
     });
     return data;
